a9301d65bf22d87bf08761a403205c236559b69c
Lexsa Campbell <lexsacampbell@lexsas-mbp.lan>
2023-02-22 11:47:52 -0800

    JSX and imported css

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { Link } from "react-router-dom";
+import '../styles/Home.css';
 
 const Home = () => {
     return (
         <>
-            <h1>Home</h1>
+            <section>
+                <div>
+                    <h1>
+                        So, you want to travel to <span>Space</span>
+                    </h1>
+
+                    <p>Let's face it; if you want to go to space, you might as well genuinely go to 
+                    outer space and not hover kind of on the edge of it. Well sit back, and relax 
+                    because we'll give you a truly out of this world experience!</p>
+                </div>
+                <div>
+                    <button>
+                        <Link to="/destination">Explore</Link>
+                    </button>
+                </div>
+            </section>
         </>
     )
 }
