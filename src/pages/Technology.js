880e7ee64a007574f5433bde223337d88ba08f7e
Lexsa Campbell <lexsacampbell@lexsas-mbp.lan>
2023-02-22 12:09:10 -0800

    JSX and imported css

--- src/pages/Technology.js
+++ src/pages/Technology.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import '../styles/Technology.css';
 
 const Technology = () => {
     return (
         <>
-            <h1>Technology</h1>
+            <section>
+                <div>
+                    <h1><span>03</span>Space launch 101</h1>
+                </div>
+
+                <div>
+                    <h2>The terminology...</h2>
+                    <h3>name</h3>
+                    <p>description</p>
+                </div>
+
+                <picture>
+                    {/* <source srcset={images.portrait} media='(min-width: 60em)' /> */}
+                    <img src={require('../images/technology/image-space-capsule-portrait.jpg')} alt='' />
+                </picture>
+            </section>
         </>
     )
 }
