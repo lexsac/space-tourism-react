112e4b7acb7158883aaea16873e7b2c0454203b7
Lexsa Campbell <lexsacampbell@lexsas-mbp.lan>
2023-02-22 11:42:27 -0800

    Page routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from './pages/Home';
+import Destination from './pages/Destination';
+import Crew from './pages/Crew';
+import Technology from './pages/Technology';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" exact element={<Home/>} />
+          <Route path="/destination" element={<Destination />} />
+          <Route path="/crew" element={ <Crew />} />
+          <Route path="/technology" element={<Technology />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
