5723997b8a3b1b9f0949f6625e420e0475d4ad11
Lexsa Campbell <lexsacampbell@lexsas-mbp.lan>
2023-02-22 11:56:55 -0800

    JSX and imported css

--- src/pages/Destination.js
+++ src/pages/Destination.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import '../styles/Destination.css';
 
 const Destination = () => {
     return (
         <>
-            <h1>Destination</h1>
+            <section>
+                <div>
+                    <h1><span>01</span>Pick your destination</h1>
+                    <img src={require('../images/destination/image-moon.png')} alt='' />
+                </div>
+                <div>
+                    <h2>Name</h2>
+                    <p>Description</p>
+
+                    <div>
+                        <div>
+                            <h3>Avg. distance</h3>
+                            <p>distance</p>
+                        </div>
+                        <div>
+                            <h3>Est. travel time</h3>
+                            <p>travel</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </>
     )
 }
