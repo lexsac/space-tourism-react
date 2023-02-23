f16ae64e7745c34fd80980d7d72ef9f470c10179
Lexsa Campbell <lexsacampbell@lexsas-mbp.lan>
2023-02-23 13:12:51 -0800

    Moved classNames from button to li elements

--- src/components/SelectionList.js
+++ src/components/SelectionList.js
@@ -5,9 +5,10 @@ const SelectionList = ({ data, page, isActive, handleClick, buttonText }) => {
       <ul className={`${page}-page__selectors`}>
         {data[page].map((item, idx) => {
           return (
-            <li key={idx}>
+            <li 
+              className={(idx === isActive) ? `${page}-page__selector active` : `${page}-page__selector`} 
+              key={idx}>
                 <button 
-                    className={(idx === isActive) ? `${page}-page__selector active` : `${page}-page__selector`} 
                     onClick={(e) => handleClick(e, idx)}>
                     {buttonText === 'index' ? idx + 1 
                         : buttonText === 'name' ? item.name
