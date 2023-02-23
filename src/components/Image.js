d9467a98d6c2c138542cf80064d0b33d518148c3
Lexsa Campbell <lexsacampbell@lexsas-mbp.lan>
2023-02-23 13:47:32 -0800

    Updated component with picture element to account for device-dependent image sizes

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,15 +1,30 @@
 import React from 'react';
 
-const Image = ({ image, page }) => {
+// const Image = ({ image, page }) => {
+//     return (
+//         <>
+//             <img 
+//                 src={image} 
+//                 className={`${page}-page__img`} 
+//                 alt='' 
+//             />
+//         </>
+//     )
+// }
+
+const Image = ({ landscapeImage, portraitImage, page, alt }) => {
     return (
-        <>
-            <img 
-                src={image} 
-                className={`${page}-page__img`} 
-                alt='' 
-            />
-        </>
-    )
-}
+      <picture className={`${page}-page__img`}>
+        <source 
+            srcSet={portraitImage} 
+            media='(min-width: 60em)' 
+        />
+        <img 
+            src={landscapeImage} 
+            alt={alt} 
+        />
+      </picture>
+    );
+  };
 
 export default Image;
