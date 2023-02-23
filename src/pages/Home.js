eeba414d5df589e2602493c95e2942187e455365
Lexsa Campbell <lexsacampbell@lexsas-mbp.lan>
2023-02-22 16:53:54 -0800

    Added classNames

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,25 +1,20 @@
 import React from 'react';
-import { Link } from "react-router-dom";
 import '../styles/Home.css';
+import Button from '../components/Button';
 
 const Home = () => {
     return (
         <>
-            <section>
+            <section className="home-page">
                 <div>
-                    <h1>
-                        So, you want to travel to <span>Space</span>
-                    </h1>
+                    <h1 className="home-page__title">So, you want to travel to <span>Space</span></h1>
 
-                    <p>Let's face it; if you want to go to space, you might as well genuinely go to 
+                    <p className="home-page__description">Let's face it; if you want to go to space, you might as well genuinely go to 
                     outer space and not hover kind of on the edge of it. Well sit back, and relax 
                     because we'll give you a truly out of this world experience!</p>
                 </div>
-                <div>
-                    <button>
-                        <Link to="/destination">Explore</Link>
-                    </button>
-                </div>
+
+                <Button />
             </section>
         </>
     )
