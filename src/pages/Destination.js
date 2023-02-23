514ccc5f0033ed532cfa69274819dbde0130361b
Lexsa Campbell <lexsacampbell@lexsas-mbp.lan>
2023-02-22 16:53:36 -0800

    Added classNames

--- src/pages/Destination.js
+++ src/pages/Destination.js
@@ -4,25 +4,27 @@ import '../styles/Destination.css';
 const Destination = () => {
     return (
         <>
-            <section>
+            <section className="destination-page">
                 <div>
-                    <h1><span>01</span>Pick your destination</h1>
-                    <img src={require('../images/destination/image-moon.png')} alt='' />
+                    <h1 className="destination-page__title"><span>01</span>Pick your destination</h1>
+                    
+                    <img className="destination-page__img" src={require('../images/destination/image-moon.png')} alt='' />
                 </div>
-                <div>
-                    <h2>Name</h2>
-                    <p>Description</p>
+                
+                <div className="destination-page__text">
+                    <h2 className="destination-page__name">Name</h2>
+                    <p className="destination-page__description">Description</p>
 
+                <div className="destination-page__info">
                     <div>
-                        <div>
-                            <h3>Avg. distance</h3>
-                            <p>distance</p>
-                        </div>
-                        <div>
-                            <h3>Est. travel time</h3>
-                            <p>travel</p>
-                        </div>
+                        <h3 className="destination-page__info-title">Avg. distance</h3>
+                        <p className="destination-page__info-data">distance</p>
                     </div>
+                    <div>
+                        <h3 className="destination-page__info-title">Est. travel time</h3>
+                        <p className="destination-page__info-data">travel</p>
+                    </div>
+                </div>
                 </div>
             </section>
         </>
