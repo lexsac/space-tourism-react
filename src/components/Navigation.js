5e42ded153ae031bc4c11726d9ee9a249fb454f9
Lexsa Campbell <lexsacampbell@lexsas-mbp.lan>
2023-02-23 10:14:19 -0800

    Added mobile nav toggle logic

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,26 +3,38 @@ import { NavLink } from 'react-router-dom';
 import '../styles/Navigation.css';
 
 const Navigation = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggle = () => setIsOpen(!isOpen);
+    const handleNavLinkClick = () => {
+        setIsOpen(false);
+    };
+
+    const combinedNavClassName = `navigation ${isOpen ? 'open' : ''}`;
+    const combinedBtnClassName = `navigation__btn-toggle ${isOpen ? 'open' : ''}`;
+    const combinedListClassName = `navigation__items ${isOpen ? 'open' : ''}`;
+
     return (
         <>
-            <nav className="navigation">
-                <ul className="navigation__items">
-                    <NavLink to="/">
+            <nav className={combinedNavClassName}>
+                <button className={combinedBtnClassName} onClick={toggle}></button>
+                <ul className={combinedListClassName}>
+                    <NavLink to="/" onClick={handleNavLinkClick}>
                         <li className="navigation__item">
                             <span>00</span>Home
                         </li>
                     </NavLink>
-                    <NavLink to="/destination">
+                    <NavLink to="/destination" onClick={handleNavLinkClick}>
                         <li className="navigation__item">
                             <span>01</span>Destination
                         </li>
                     </NavLink>
-                    <NavLink to="/crew">
+                    <NavLink to="/crew" onClick={handleNavLinkClick}>
                         <li className="navigation__item">
                             <span>02</span>Crew
                         </li>
                     </NavLink>
-                    <NavLink to="/technology">
+                    <NavLink to="/technology" onClick={handleNavLinkClick}>
                         <li className="navigation__item">
                             <span>03</span>Technology
                         </li>
