852627226db5309998d088cbb580b28d78d6c695
Lexsa Campbell <lexsacampbell@lexsas-mbp.lan>
2023-02-23 18:28:55 -0800

    Added additional classNames for desktop styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
     return (
         <>
             <section className="home-page">
-                <div>
+                <div className="home-page__info">
                     <h1 className="home-page__title">So, you want to travel to <span>Space</span></h1>
 
                     <p className="home-page__description">Let's face it; if you want to go to space, you might as well genuinely go to 
@@ -14,7 +14,7 @@ const Home = () => {
                     because we'll give you a truly out of this world experience!</p>
                 </div>
 
-                <Button />
+                <Button className="home-page__button" />
             </section>
         </>
     )
